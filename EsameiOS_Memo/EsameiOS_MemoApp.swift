82113564ff488a9500f241990d56345892182ea5
francesco-riva <francesco.riva@itsrizzoli.it>
2025-10-17 14:18:37 +0200

    Initial Commit

---
+++ EsameiOS_Memo/EsameiOS_MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsameiOS_MemoApp.swift
+//  EsameiOS_Memo
+//
+//  Created by d3 on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EsameiOS_MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
